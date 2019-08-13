495611ee88fc5bfe82c5acb17b3e8525dda03d20
Adeel Imran <adeelimranr@gmail.com>
2019-08-13 12:01:08 +0200

    feat(src/index.js): [CxX-XXXX] Initialized the main module to log out star wars character names

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const STAR_WAR_CHARACTER_NAMES = [
+    'Almec',
+    'Amee',
+    'Kitster Banai',
+    'Darth Bane',
+    'Moradmin Bast'
+];
+
+console.log('Some names are', STAR_WAR_CHARACTER_NAMES.join(', '));
