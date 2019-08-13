5bccc7a4b6a0e81ea5f9dd7b178b5d645051636d
Adeel Imran <adeelimranr@gmail.com>
2019-08-13 18:16:07 +0200

    feat(semantic): bREAKING CHANGE: a fake breaking change
    BREAKING CHANGE: A fake description of the breaking change

--- .releaserc.js
+++ .releaserc.js
@@ -1,20 +1,21 @@
 module.exports = {
   plugins: [
+    // [
+    //   '@semantic-release/commit-analyzer', {
+    //     preset: 'angular',
+    //     releaseRules: [
+    //       { "type": "docs", "scope": "README", "release": "patch" },
+    //       { "type": "refactor", "release": "patch" },
+    //       { "type": "style", "release": "patch" }
+    //     ],
+    //     parserOpts: {
+    //       noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
+    //     }
+    //   }
+    // ],
     [
-      '@semantic-release/commit-analyzer', {
-        preset: 'angular',
-        releaseRules: [
-          { "type": "docs", "scope": "README", "release": "patch" },
-          { "type": "refactor", "release": "patch" },
-          { "type": "style", "release": "patch" }
-        ],
-        parserOpts: {
-          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"]
-        }
-      }
-    ],
-    [
-      'semantic-release/release-notes-generator', {
+      'semantic-release/release-notes-generator',
+      {
         preset: 'angular',
         parserOpts: {
           "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
