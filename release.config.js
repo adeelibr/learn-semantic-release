77054374ae8f0677d1534becc99fc0ba6591ba7e
Adeel Imran <adeelimranr@gmail.com>
2019-08-14 12:49:30 +0200

    feat(semantic): :racehorse: change preset for semantic to atom style

--- release.config.js
+++ release.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer', // to analyze commits & assign semantic vers accordingly
+    // to analyze commits & assign semantic vers accordingly
+    ['@semantic-release/commit-analyzer', {
+      presets: 'atom',
+    }],
+    // to create/update changelog content
+    ['@semantic-release/release-notes-generator', {
+      preset: 'atom',
+    }],
     '@semantic-release/error',
-    '@semantic-release/github',    
-    '@semantic-release/release-notes-generator', // to generate changelog content
+    '@semantic-release/github',
     '@semantic-release/changelog', // plugin to create or update a changelog file
     '@semantic-release/git',
   ],
