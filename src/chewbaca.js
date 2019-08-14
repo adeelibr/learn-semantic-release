dfbf3c3695ef410d371a0ed1869763b1cfd70dd5
Adeel Imran <adeelimranr@gmail.com>
2019-08-14 14:03:15 +0200

    feat(chewbaca): add chewbaca dance like michael jackson module :tada: because why not
    A chewbaca module that makes chewbaca do dance moves

--- src/chewbaca.js
+++ src/chewbaca.js
@@ -1,3 +1,7 @@
 export const sayChewbacaDialogues = function () {
   console.log('Raarghhh! Vaaarrk! Nowrrrrragh!');
 };
+
+export const makeChewbaceDance = () => {
+  console.log('🕴🕺 🕴🕺 🕴🕺 🕴🕺 🕴🕺 🕴🕺 🕴🕺 ');
+};
