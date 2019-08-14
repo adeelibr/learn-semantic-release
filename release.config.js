b433ae509e135c4a34731d6261db152d11491af1
Adeel Imran <adeelimranr@gmail.com>
2019-08-14 12:37:18 +0200

    fix(semantic): add semantic release plugins

--- release.config.js
+++ release.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer', //  plugin to analyze commits & assign semantic vers accordingly
-    '@semantic-release/release-notes-generator', // plugin to generate changelog content
+    '@semantic-release/commit-analyzer', // to analyze commits & assign semantic vers accordingly
+    '@semantic-release/error',
+    '@semantic-release/github',    
+    '@semantic-release/release-notes-generator', // to generate changelog content
     '@semantic-release/changelog', // plugin to create or update a changelog file
     '@semantic-release/git',
   ],
