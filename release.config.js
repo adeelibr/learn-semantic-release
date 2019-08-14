9c2f5a97bac4d93d3ca9e0caa2309297a352d74b
Adeel Imran <adeelimranr@gmail.com>
2019-08-14 11:59:17 +0200

    fix(semantic/travis): change releease.config and travis.yml file

--- release.config.js
+++ release.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    // '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    '@semantic-release/commit-analyzer', //  plugin to analyze commits & assign semantic vers accordingly
+    '@semantic-release/release-notes-generator', // plugin to generate changelog content
+    '@semantic-release/changelog', // plugin to create or update a changelog file
     '@semantic-release/git',
   ],
   branch: 'master',
