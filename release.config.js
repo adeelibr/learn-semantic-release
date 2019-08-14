44cd3144d2ad11bdf36d6fa03a6ec2f5cfa2ec41
Adeel Imran <adeelimranr@gmail.com>
2019-08-14 12:59:43 +0200

    fix(semantic): :tada: fix release.config.js syntax error

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     // to analyze commits & assign semantic vers accordingly
     ['@semantic-release/commit-analyzer', {
-      presets: 'atom',
+      preset: 'atom',
     }],
     // to create/update changelog content
     ['@semantic-release/release-notes-generator', {
